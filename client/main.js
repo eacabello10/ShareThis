b1cb04af515acf6aa37b530966a274a7ce16fe92
JairoBm13 <j.bautista.m13@outlook.com>
2017-12-09 18:00:43 -0500

    removed trash

--- client/main.js
+++ client/main.js
@@ -5,9 +5,6 @@ import {render} from "react-dom";
 //Import del imports/ui
 import App from "../imports/ui/App.jsx";
 
-//Config de users
-import '../imports/startup/accounts-config.js';
-
 Meteor.startup(()=> {
   render(<App />, document.getElementById("render-target"));
 });
